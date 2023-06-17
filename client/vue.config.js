79cc9b3efd309b652e21f7c0ce5d8e9f45d088a2
Mr. Shubham <gunjalshubham2@gmail.com>
2023-06-17 22:54:17 +0530

    test

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/GunjalShubham2.github.io/',
   transpileDependencies: true,
   configureWebpack: {
     devServer: {
